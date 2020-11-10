2be4dd4c33ca3f7a01dbe0e1241abff757cd54d3
ByO <bayronortiz89@gmail.com>
2020-11-09 20:26:42 -0600

    creando una consulta a la BD y enviando respuestas por ajax

--- includes/models/model-admin.php
+++ includes/models/model-admin.php
@@ -19,15 +19,34 @@
     // 2. hasheamos la el password
     $hash_password = password_hash($password, PASSWORD_BCRYPT, $options);    
 
-    $output = [
-      'pass' => $hash_password
-    ];
+    // importamos la conexion a la BD
+    require '..\functions\db_connect.php';
 
-  }
+    try {
+      // realizar la consulta a la BD
+      $stmt = $conn->prepare('INSERT INTO user (username, password) VALUES  (?, ?)');
+      $stmt->bind_param('ss', $usuario, $hash_password);
+      $stmt->execute();
+
+      $output = [
+        'response' => 'success',
+        'id' => $stmt->insert_id,
+        'action' => $accion
+        // 'error' => $stmt->error
+      ];
+
+      $stmt->close();
+      $conn->close();
+    }catch(Exception $e) {
+      // en caso de error, tomar la exception
+      $output = [
+        'response' => $e->getMessage()
+      ];
+    };
+  };
 
   if ($accion === 'login') {
-    // datos de acceso
-    
+    // datos de acceso    
   }
 
   echo json_encode($output);
