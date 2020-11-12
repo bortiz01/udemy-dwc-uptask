38e3a34d949a5f2864681c84df883982b407e0ee
ByO <bayronortiz89@gmail.com>
2020-11-12 16:44:37 -0600

    agregando la funcionalidad para crear nuevos proyectos

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,7 @@
 // todo el codigo js se ejecutara despues de que termine de cargarse todos los elementos del DOM(HTML)
 document.addEventListener("DOMContentLoaded", function () {
   eventListener();
+  const listProjects = document.querySelector("ul#proyectos");
 
   function eventListener() {
     document.querySelector(".crear-proyecto a").addEventListener("click", newProject);
@@ -8,6 +9,35 @@ document.addEventListener("DOMContentLoaded", function () {
 
   function newProject(e) {
     // e.preventDefault();
-    console.log("presionaste un nuevo proyecto");
+
+    // crea un <input> para el nombre del nuevo proyecto
+    // creamos el elemento 'li'
+    let newProject = document.createElement("li");
+
+    // adicionamos al elemto 'li' un '<input>'
+    newProject.innerHTML = "<input type='text' id='new-project'>";
+
+    // adicionamos el elemento 'li' creado anteriormente debajo de elemento 'ul#proyectos'
+    listProjects.appendChild(newProject);
+
+    // seleccionar el ID del nuevo elemento
+    const inputNewProject = document.querySelector("#new-project");
+
+    // al presionar ENTER creamos el proyecto
+    inputNewProject.addEventListener("keypress", function (e) {
+      // keypress toma el valor de which o keycode del elemento
+      const keypress = e.which || e.keycode;
+      // verificamos si se presiono ENTER (13)
+      if (keypress === 13) {
+        // salvamos el nombre del proyecto en la DB
+        saveProjectDB(inputNewProject.value);
+        // removemos el elemento creado
+        listProjects.removeChild(newProject);
+      }
+    });
+  }
+
+  function saveProjectDB(p_projectName) {
+    console.log(p_projectName);
   }
 });
