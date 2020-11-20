adc5a1f9c7d442d29693c6a7188ac5c1ecd45fbf
ByO <bayronortiz89@gmail.com>
2020-11-19 19:07:31 -0600

    guardando el estado de las tareas en la bd

--- includes/models/model-task.php
+++ includes/models/model-task.php
@@ -12,6 +12,7 @@
     
     // importamos la conexion a la BD
     require '..\functions\db_connect.php';
+  
 
     try {
       // definimos la consulta por prepare statement      
@@ -59,12 +60,57 @@
 
   /* ------------------------------- update task ------------------------------ */
   if ($action === 'actualizar') {
-    echo json_encode($_POST);
-  }
+    // obtener valores
+    $id_task = (int) $_POST['id_task'];    
+    $state = (int) $_POST['state']; //indicamos que el valor sera un entero
+    
+    // importamos la conexion a la BD
+    require '..\functions\db_connect.php';
+
+    try {
+      // definimos la consulta por prepare statement      
+      // 1. indicamos la consulta con parametros
+      $stmt = $conn->prepare('UPDATE tasks SET state = ? WHERE id_task = ?');
+      
+      // 2. relacionamos los parametros
+      $stmt->bind_param('ii', $state, $id_task);
+      
+      // 3. ejecutamos la consulta
+      $stmt->execute();
+      
+      // 4. procesamos el resultado de la consulta
+      // si se inserto un registro
+      if ($stmt->affected_rows > 0) {
+        // creamos la respuesta a enviar
+        $output = [
+          'status' => 'success'
+        ];
+      // si no se modifico ningun registro
+      } else {
+        // creamos la respuesta a enviar
+        $output = [
+          'status' => 'error',
+          'error' => $stmt->error
+        ];
+      };   
+      
+      // 5. cerramos el statement
+      $stmt->close();    
+
+      // 6. cerramos la conexion
+      $conn->close();
+    }catch(Exception $e) {
+      // en caso de error, tomar la exception
+      $output = [
+        'response' => 'error',
+        'error' => $e->getMessage()
+      ];
+    };
+  };
 
   /* --------------------------- return the response -------------------------- */
   // convertimos el arreglo en un JSON para el 
   // intercambio de datos entre PHP y AJAX
-  // echo json_encode($output);
+  echo json_encode($output);
 
 ?>
